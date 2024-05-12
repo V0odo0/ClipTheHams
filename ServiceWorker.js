65de1e3f61995eb69d2eac22de8102587008cfc0
V0odo0 <Voodoo2211@gmail.com>
2024-05-13 00:50:11 +0500

    ton connect test

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "V0odo0-ClipTheHams-0.1.0";
+const contentToCache = [
+    "Build/ClipTheHams.loader.js",
+    "Build/ClipTheHams.framework.js.unityweb",
+    "Build/ClipTheHams.data.unityweb",
+    "Build/ClipTheHams.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
